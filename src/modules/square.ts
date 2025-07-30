11c8c02aa5493515afdfc9e87c00f40a87ecde93
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-30 18:02:50 +0545

    add endX and endY property to square

--- src/modules/square.ts
+++ src/modules/square.ts
@@ -4,20 +4,29 @@ export class Square {
   public height: number;
   public width: number;
   public weight: number;
+  public speed: number;
+  public endX: number;
+  public endY: number;
+
   constructor({
     startX,
     startY,
     width,
     weight,
+    speed,
   }: {
     startX: number;
     startY: number;
     width: number;
     weight: number;
+    speed: number;
   }) {
     this.startX = startX;
     this.startY = startY;
     this.height = this.width = width;
+    this.endX = this.startX + this.width;
+    this.endY = this.startY + this.height;
     this.weight = weight;
+    this.speed = speed ?? 1;
   }
 }
